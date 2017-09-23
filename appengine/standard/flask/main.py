f5b4b75958eef4d6d54e7ba50deea348dbda2b44
Rachel Oei <racheloei@users.noreply.github.com>
2017-09-23 17:24:20 -0500

    Add home page

--- appengine/standard/flask/main.py
+++ appengine/standard/flask/main.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+  return render_template('index.html')
+
 @app.route('/form')
 def form():
   return render_template('form.html')

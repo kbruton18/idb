d0d116ecf8871ae576223ff6dafe54a4861aed40
Rachel Oei <rachel.oei@utexas.edu>
2017-09-25 20:26:05 -0500

    fixed flask for index and about, changed titles, deleted unusued files

--- appengine/standard/flask/main.py
+++ appengine/standard/flask/main.py
@@ -3,18 +3,11 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index.html')
 def index():
   return render_template('index.html')
 
-@app.route('/form')
-def form():
-  return render_template('form.html')
+@app.route('/about.html')
+def about():
+  return render_template('about.html')
 
-@app.route('/submitted', methods=['POST'])
-def submitted_form():
-  name = request.form['name']
-  email = request.form['email']
-  return render_template(
-    'submitted_form.html',
-    name=name,
-    email=email)

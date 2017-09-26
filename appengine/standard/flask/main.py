9b51e7b608d9046592e2d98d432284e5d34246ed
Rachel Oei <rachel.oei@utexas.edu>
2017-09-25 21:08:20 -0500

    made templates for models

--- appengine/standard/flask/main.py
+++ appengine/standard/flask/main.py
@@ -10,4 +10,23 @@ def index():
 @app.route('/about.html')
 def about():
   return render_template('about.html')
+  
+@app.route('/countries.html')
+def countries():
+  return render_template('countries.html')
+  
+@app.route('/cities.html')
+def cities():
+  return render_template('cities.html')
+  
+@app.route('/placesofinterest.html')
+def placesofinterest():
+  return render_template('placesofinterest.html')
+  
+@app.route('/placestostay.html')
+def placestostay():
+  return render_template('placestostay.html')
 
+@app.route('/countries/china.html')
+def china():
+  return render_template('countries/china.html')

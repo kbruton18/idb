dc6e6bb236e43352c73c984e14dbadf8763cc694
Ryan Le <ryanhle@utexas.edu>
2017-09-27 16:05:17 -0500

    Individual campgrounds

--- appengine/standard/flask/main.py
+++ appengine/standard/flask/main.py
@@ -10,19 +10,19 @@ def index():
 @app.route('/about.html')
 def about():
   return render_template('about.html')
-  
+
 @app.route('/countries.html')
 def countries():
   return render_template('countries.html')
-  
+
 @app.route('/cities.html')
 def cities():
   return render_template('cities.html')
-  
+
 @app.route('/placesofinterest.html')
 def placesofinterest():
   return render_template('placesofinterest.html')
-  
+
 @app.route('/states.html')
 def states():
   return render_template('states.html')
@@ -50,3 +50,16 @@ def yellowstone():
 @app.route('/states/wyoming.html')
 def yellowstone():
   return render_template('states/wyoming.html')
+
+@app.route('/campgrounds/2357')
+def campground1():
+  return render_template('campgrounds.html', name='Mammoth Campground', park='Yellowstone National Park', state='Wyoming', description="Our only year-round campground, Mammoth is located five miles south of Gardiner, Montana and the park's North Entrance. Situated in a high sage brush steppe above the Gardner River, scattered juniper and Douglas fir trees provide shade during hot summer months. The campground is close to fishing, hiking, and the famous Mammoth Hot Springs Terraces. Great wildlife viewing opportunities abound with elk and bison occasionally passing through the campground.", sites='85')
+
+@app.route('/campgrounds/2372')
+def campground2():
+  return render_template('campgrounds.html', name='Madison Campground', park='Yellowstone National Park', state='Wyoming', description="One of our most popular campgrounds due to its central location and long season, Madison sits about 14 miles east from the town of West Yellowstone and 16 miles north of Old Faithful. Nearby, the Gibbon and Firehole rivers join to form the Madison, all of which are considered world-class fly fishing destinations. In spring and early summer, nearby meadows teem with wildflowers and grazing bison. In September and early October, you can often hear the bugling of bull elk.", sites='278')
+
+
+@app.route('/campgrounds/2372')
+def campground2():
+  return render_template('campgrounds.html', name='Fishing Bridge RV Park', park='Yellowstone National Park', state='Wyoming', description="Fishing Bridge RV Park is located near the Yellowstone River where it exits Yellowstone Lake on its way north toward the Grand Canyon of the Yellowstone. This is the only campground in Yellowstone to offer water, sewer, and electrical hookups for RVs. Because grizzly bears frequent the area, no tents or tent campers are allowed. Campfires are prohibited in the Fishing Bridge RV Park, including the use of portable fire pits.", sites='340')

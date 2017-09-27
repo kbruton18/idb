64246295899dbd79c1a1b7a2bdad274466fbd58e
Ryan Le <ryanhle@utexas.edu>
2017-09-27 16:15:55 -0500

    Renamed the last (plz) function

--- appengine/standard/flask/main.py
+++ appengine/standard/flask/main.py
@@ -60,6 +60,6 @@ def campground2():
   return render_template('campgrounds.html', name='Madison Campground', park='Yellowstone National Park', state='Wyoming', description="One of our most popular campgrounds due to its central location and long season, Madison sits about 14 miles east from the town of West Yellowstone and 16 miles north of Old Faithful. Nearby, the Gibbon and Firehole rivers join to form the Madison, all of which are considered world-class fly fishing destinations. In spring and early summer, nearby meadows teem with wildflowers and grazing bison. In September and early October, you can often hear the bugling of bull elk.", sites='278')
 
 
-@app.route('/campgrounds/2372')
-def campground2():
+@app.route('/campgrounds/2374')
+def campground3():
   return render_template('campgrounds.html', name='Fishing Bridge RV Park', park='Yellowstone National Park', state='Wyoming', description="Fishing Bridge RV Park is located near the Yellowstone River where it exits Yellowstone Lake on its way north toward the Grand Canyon of the Yellowstone. This is the only campground in Yellowstone to offer water, sewer, and electrical hookups for RVs. Because grizzly bears frequent the area, no tents or tent campers are allowed. Campfires are prohibited in the Fishing Bridge RV Park, including the use of portable fire pits.", sites='340')

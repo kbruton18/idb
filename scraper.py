ef94acdc37bccdf02de185a5f89300fdebcfec50
Katherine Bruton <kbruton@utexas.edu>
2017-10-22 14:03:44 -0500

    trivial json parser

--- scraper.py
+++ scraper.py
@@ -1,13 +1,18 @@
 import urllib.request, json
+# from pprint import pprint
 
-# Configure API request
-endpoint = "https://developer.nps.gov/api/v1/parks?limit=600&api_key=API_KEY_HERE"
+# parks request
+endpoint = "https://developer.nps.gov/api/v1/parks?limit=1000&api_key=api_key_goes_here"
 HEADERS = {}
 req = urllib.request.Request(endpoint,headers=HEADERS)
 
 response = urllib.request.urlopen(req)
 the_page = response.read()
+# response.read() returns bytes, which we need to decode into a string
+the_page = the_page.decode("utf-8") 
 
+data = json.loads(the_page)
 
-# this will print out everything in json format
-print(str(the_page))
+# this will print out each individual entry in data
+for x in data["data"]:
+  print (x)

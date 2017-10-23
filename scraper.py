421d7b5be5345b0c6ba7e3ef95c01a7f97f25a88
Rachel Oei <racheloei@users.noreply.github.com>
2017-10-22 22:51:59 -0500

    Change scraper.py to Python 2

--- scraper.py
+++ scraper.py
@@ -1,12 +1,11 @@
-import urllib.request, json
+import urllib2, json
 # from pprint import pprint
 
 # parks request
 endpoint = "https://developer.nps.gov/api/v1/parks?limit=1000&api_key=api_key_goes_here"
-HEADERS = {}
-req = urllib.request.Request(endpoint,headers=HEADERS)
+req = urllib.Request(endpoint,headers={})
 
-response = urllib.request.urlopen(req)
+response = urllib2.urlopen(req)
 the_page = response.read()
 # response.read() returns bytes, which we need to decode into a string
 the_page = the_page.decode("utf-8") 

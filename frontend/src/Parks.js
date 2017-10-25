21538d374ba642ffa689424ad0f10a934fed4f3f
Derek Chang <derekchang109@gmail.com>
2017-10-25 14:00:13 -0500

    basic idea for parsing but need to have for multiple

--- frontend/src/Parks.js
+++ frontend/src/Parks.js
@@ -16,49 +16,56 @@ import {
   CardFooter
 } from 'reactstrap';
 import ParkDetail from './ParkDetail.js';
+import CustomCard from './CustomCard.js';
 
 const y = require('./img/parks/yosemite.jpg');
 
-// class Parks extends Component {
+class ParkCard extends Component {
 
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       id: props.params.id, 
-//       data: []
-//     }
-//   }
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      states: "",
+      parkCode: "",
+      url: "",
+      desig: ""
+    }
+  }
 
-//   componentDidMount() {
-//     fetch('https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=ZpESFe8R2hqjdYKmaXyiblZZeaKuYhW1l8q6WmO2')
-//       .then((response) => response.json())
-//       .then((responseJson) => {
-//         this.setState({
-//           data: responseJson
-//         })
-//       })
-//   }
+  componentDidMount() {
+    fetch('https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=ZpESFe8R2hqjdYKmaXyiblZZeaKuYhW1l8q6WmO2')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({
+          name: responseJson['data'][0]['name'],
+          states: responseJson['data'][0]['states'],
+          parkCode: responseJson['data'][0]['parkCode'],
+          url: responseJson['data'][0]['url'],
+          desig: responseJson['data'][0]['designation']
+        })
+      })
+  }
 
-//   render() {
-//     const p = this.state.data.data.map((parks) => {
-//       return (
-//           <a href="#" className="list-parks" key = {parks.name}> 
-//           {parks.name}
-//           </a>
-//         )
-//     });
-//     return (
-//       <div>
-//         <h1>Parks page</h1>
-//         <div className= "list-parks">
-//           {p}
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// export default Parks
+  render() {
+    const yosemite = {"name": this.state.name, "state": this.state.states, "code": this.state.parkCode, "desg": this.state.designation, "visit": "Yosemite Valley Visitor Center", 
+    "url": this.state.url, imageSrc: y, imageCaption: "Derek"};
+    return (
+      <Container className="bg-faded p-4 my-4">
+      <hr className="divider"/>
+      <h2 className="text-center text-lg text-uppercase my-0">
+        parks
+      </h2>
+      <hr className="divider"/>
+      <Row>
+        <Col lg="4" md="6" sm="12">
+          <AboutPark park={yosemite}/>
+        </Col>
+      </Row>
+    </Container>
+    );
+  }
+}
 
 function AboutPark(props) {
   return (
@@ -80,29 +87,10 @@ function AboutPark(props) {
   )
 }
 
-class Cards extends Component {
-  render() {
-    const yosemite = {"name": "Yosemite", "state": "CA", "code": "yose", "desg": "National Park", "visit": "Yosemite Valley Visitor Center", "url": "https://www.nps.gov/yose/index.htm", imageSrc: y, imageCaption: "Derek"};
-  return (
-    <Container className="bg-faded p-4 my-4">
-      <hr className="divider"/>
-      <h2 className="text-center text-lg text-uppercase my-0">
-        parks
-      </h2>
-      <hr className="divider"/>
-      <Row>
-        <Col lg="4" md="6" sm="12">
-          <AboutPark park={yosemite}/>
-        </Col>
-      </Row>
-    </Container>
-    );
-  }
-}
 
 const Parks = (props) => (
     <div> 
-      <Route exact path="/parks" component={Cards}/>    
+      <Route exact path="/parks" component={ParkCard}/>    
       <Route path="/parks/:id" component={ParkDetail}/>    
     </div>
   )

c48853874d8676d84029833c0b1e6d9fa68879ce
Rachel Oei <rachel.oei@utexas.edu>
2017-10-26 14:57:08 -0500

    got rid of all state wikipedia parsing ugliness

--- appengine/standard/flask/add_states.py
+++ appengine/standard/flask/add_states.py
@@ -4,11 +4,25 @@ import requests
 from main import create_app
 from pprint import pprint
 
+def clean_text(text):
+  text = text.replace("[", "").replace("]", "")
+  endIndex = text.find("<")
+  if endIndex != -1:
+  	text = text[0:endIndex]
+  endIndex = text.find("|")
+  if endIndex != -1:
+  	text = text[0:endIndex]
+  endIndex = text.find("(")
+  if endIndex != -1:
+  	text = text[0:endIndex]
+  text = text.replace(")", "")
+  return text
+
 create_app().app_context().push()
 
 """Simple request handler that shows all of the MySQL variables."""
 # parks request
-endpoint = "https://en.wikipedia.org/w/api.php?action=query&titles=Florida|New%20York%20(state)&prop=revisions&rvprop=content&format=json"
+endpoint = "https://en.wikipedia.org/w/api.php?action=query&titles=Florida&prop=revisions&rvprop=content&format=json"
 req = urllib2.Request(endpoint,headers={"User-Agent": "UT Austin CS 373 Project (http://swetravels.me/)"})
 
 response = urllib2.urlopen(req)
@@ -23,10 +37,7 @@ pageids = data["query"]["pages"].keys()
 for id in pageids:
   statedata = data["query"]["pages"][id]
   
-  name = statedata["title"]
-  nameIndex = name.find("(")
-  if nameIndex != -1:
-    name = name[0:nameIndex]
+  name = clean_text(statedata["title"])
   nickname = ""
   abbreviations = ""
   timeZone = ""
@@ -45,55 +56,37 @@ for id in pageids:
   for attribute in attributes:
     if nickname == "" and attribute.startswith("|Nickname"):
       index = attribute.find("=")
-      nickname = attribute[index+2:].replace("[", "").replace("]", "")
-      endIndex = nickname.find("|")
-      if endIndex != -1:
-        nickname = nickname[0:endIndex]
+      nickname = clean_text(attribute[index+2:])
     if abbreviations == "" and attribute.startswith("|PostalAbbreviation"):
       index = attribute.find("=")
-      abbreviations = attribute[index+2:]
+      abbreviations = clean_text(attribute[index+2:])
     if timeZone == "" and attribute.startswith("|TimeZone "):
       index = attribute.find("=")
-      timeZone = attribute[index+2:].replace("[", "").replace("]", "")
-      endIndex = timeZone.find("|")
-      if endIndex != -1:
-        timeZone = timeZone[0:endIndex]
+      timeZone = clean_text(attribute[index+2:])
     if governor == "" and attribute.startswith("|Governor "):
       index = attribute.find("=")
-      governor = attribute[index+2:].replace("[", "").replace("]", "")
-      endIndex = governor.find("(")
-      if endIndex != -1:
-        governor = governor[0:endIndex]
+      governor = clean_text(attribute[index+2:])
     if capital == "" and attribute.startswith("|Capital "):
       index = attribute.find("=")
-      capital = attribute[index+2:].replace("[", "").replace("]", "")
-      endIndex = capital.find("|")
-      if endIndex != -1:
-        capital = capital[0:endIndex]
+      capital = clean_text(attribute[index+2:])
     if largestCity == "" and attribute.startswith("|LargestCity"):
       index = attribute.find("=")
-      largestCity = attribute[index+2:].replace("[", "").replace("]", "")
-      endIndex = largestCity.find("|")
-      if endIndex != -1:
-        largestCity = largestCity[0:endIndex]
+      largestCity = clean_text(attribute[index+2:])
+      if "pital" in largestCity:
+        largestCity = capital
     if totalPopulation == "" and attribute.startswith("|2010Pop"):
       index = attribute.find("=")
-      endIndex = attribute.find("<ref")
-      if endIndex != -1:
-        totalPopulation = attribute[index+2:endIndex]
+      totalPopulation = clean_text(attribute[index+2:])
     if totalArea == "" and attribute.startswith("|TotalAreaUS"):
       index = attribute.find("=")
-      endIndex = attribute.find("<ref")
-      if endIndex != -1:
-        totalArea = attribute[index+2:endIndex]
+      totalArea = clean_text(attribute[index+2:])
     if medianIncome == "" and attribute.startswith("|MedianHouseholdIncome"):
       index = attribute.find("=")
-      endIndex = attribute.find("<ref")
-      if endIndex != -1:
-        medianIncome = attribute[index+2:endIndex]
+      medianIncome = clean_text(attribute[index+2:])
     if url == "" and attribute.startswith("|Website"):
       index = attribute.find("=")
       url = attribute[index+2:]
+      
   state = State(name, abbreviations, nickname, timeZone, governor, capital, largestCity, totalPopulation, totalArea, medianIncome, nationalParks, campgrounds, url)
   pprint(state.__dict__)
 print('Finished executing\n')

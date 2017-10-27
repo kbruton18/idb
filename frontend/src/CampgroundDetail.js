946fb9a47dd18f351f1524e1a3ff967b34ea7ea0
Linh Nguyen <linh-nguyen@utexas.edu>
2017-10-26 20:09:50 -0500

    Display all campground details

--- frontend/src/CampgroundDetail.js
+++ frontend/src/CampgroundDetail.js
@@ -1,4 +1,14 @@
 import React, {Component} from 'react';
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle
+} from 'reactstrap';
 import CustomCard from './CustomCard.js';
 
 class CampgroundDetail extends Component {
@@ -12,7 +22,7 @@ class CampgroundDetail extends Component {
   }
 
   componentDidMount() {
-    fetch(this.state.id)
+    fetch('http://sweet-travels.appspot.com/api/campgrounds/' + this.state.id)
       .then((response) => response.json())
       .then((responseJson) => {
         this.setState({
@@ -23,9 +33,23 @@ class CampgroundDetail extends Component {
 
   render() {
     return (
-      <div>
-        <CustomCard title={this.state.id}/>
-      </div>
+      <Card>
+        <CardImg top width="75%" src={this.state.data.imageUrl} alt="campground URL" />
+        <CardBody>
+          <CardText>
+            <b>Park Code: </b>{this.state.data.parkCode}<br />
+            <b>State(s): </b>{this.state.data.states}<br />
+            <b>Description: </b>{this.state.data.description}<br />
+            <b>Regulations Overview: </b>{this.state.data.regulationsOverview}<br />
+            <b>Regulations URL: </b><a href={this.state.data.regulationsUrl}>{this.state.data.regulationsUrl}</a><br />
+            <b>Wheelchair Access: </b>{this.state.data.wheelchairAccess}<br />
+            <b>Internet Info: </b>{this.state.data.internetInfo}<br />
+            <b>Total Sites: </b>{this.state.data.totalSites}<br />
+            <b>Directions Info: </b>{this.state.data.directionsInfo}<br />
+            <b>Directions URL:</b> <a href={this.state.data.directionsUrl}>{this.state.data.directionsUrl}</a>
+          </CardText>
+        </CardBody>
+      </Card>
     );
   }
 }

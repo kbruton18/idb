0c8c889ce84089e26e3c48df27d5327555521661
Derek Chang <derekchang109@gmail.com>
2017-10-26 19:21:49 -0500

    states model

--- appengine/standard/flask/api.py
+++ appengine/standard/flask/api.py
@@ -54,6 +54,7 @@ def get_states_dict():
         state_dict["nationalParks"] = state.nationalParks
         state_dict["campgrounds"] = state.campgrounds
         state_dict["url"] = state.url
+        state_dict["imageUrl"] = state.imageUrl
         states[state.abbreviations] = state_dict
     return states
 
--- frontend/src/States.js
+++ frontend/src/States.js
@@ -15,52 +15,66 @@ import {
 } from 'reactstrap';
 import StateDetail from './StateDetail.js';
 
-const ca = require('./img/states/california.jpg');
+class StateCard extends Component {
 
-function StateCard(props) {
-  return (
-    <Card className="text-center">
-      <Link to={`/states/${props.title}`}>
-        <CardImg top width="100%" src={props.img} alt={props.alt} />
-      </Link>
-      <CardBody>
-        <CardTitle>{props.title}</CardTitle>
-        <CardText>
-          <b>Abbreviations: </b>{props.abbreviation}<br />
-          <b>Nickname(s): </b>{props.nickname}<br />
-          <b>Timezone: </b>{props.timezone}<br />
-          <b>Capital: </b>{props.capital}<br />
-          <b>National Park(s):</b> <a href={props.parkUrl}>{props.parkName}</a>
-        </CardText>
-      </CardBody>
-    </Card>
-  );
-}
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    }
+  }
 
-function StateLanding(props) {
-  return (
-    <div>
+  componentDidMount() {
+    fetch('http://sweet-travels.appspot.com/api/states')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({
+          data: responseJson
+        })
+      })
+  }
+
+  render() {
+    const states = this.state.data.map((d) => {
+      return (
+        <Col lg="4" md="6" sm="12">
+        <Card className ="text-center">
+          <Link to={`/states/${d.abbreviations}`}>
+            <CardImg top width="100%" src={d.imageUrl} alt="ok" />
+          </Link>
+          <CardBody>
+            <CardTitle className="text-center">{d.name}</CardTitle>
+            <CardText>
+            <b>Abbreviations: </b>{props.abbreviations}<br />
+            <b>Nickname(s): </b>{props.nicknames}<br />
+            <b>Timezone: </b>{props.timeZone}<br />
+            <b>Capital: </b>{props.capital}<br />
+            <b>National Park(s):</b> <Link to='/parks/${props.nationalParks}'>{props.nationalParks}</Link>
+            </CardText>
+          </CardBody>
+        </Card>
+        </Col>
+        )
+    })
+    return (
       <Container className="bg-faded p-4 my-4">
-        <hr className="divider"/>
-        <h2 className="text-center text-lg text-uppercase my-0">States</h2>
-        <hr className="divider"/>
-        <Row>
-          <Col lg="4" md="6" sm="12">
-            <StateCard img={ca} alt="California Flag" title="California" abbreviation="CA, Calif., Cal." 
-              nickname="The Golden State" timezone="Pacific (UTC −8/−7)" 
-              capital="Sacramento" parkUrl="http://www.swetravels.me/parks/yosemite.html"
-              parkName="Yosemite"/>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
+      <hr className="divider"/>
+      <h2 className="text-center text-lg text-uppercase my-0">
+        parks
+      </h2>
+      <hr className="divider"/>
+      <Row>
+          {states}
+      </Row>
+    </Container>
+    );
+  }
 }
 
 export default function States(props) {
   return (
     <div>
-      <Route exact path="/states" component={StateLanding}/>
+      <Route exact path="/states" component={StateCard}/>
       <Route path="/states/:id" component={StateDetail}/>
     </div>
   )

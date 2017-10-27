e941ddd2a8c97c770f85a2afd1358e9b47cb69f3
Linh Nguyen <linh-nguyen@utexas.edu>
2017-10-26 20:37:42 -0500

    Updated states detail pages

--- frontend/src/StateDetail.js
+++ frontend/src/StateDetail.js
@@ -1,18 +1,31 @@
-import React, {Component} from 'react';
-import CustomCard from './CustomCard.js';
+import React, { Component } from 'react';
+import {
+  Link,
+  Route
+} from 'react-router-dom';
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle
+} from 'reactstrap';
 
 class StateDetail extends Component {
 
-  constructor({match}) {
-    super(match);
+  constructor(props) {
+    super(props);
     this.state = {
-      id: match.params.id, 
+      id: props.match.params.id,
       data: []
     }
   }
 
   componentDidMount() {
-    fetch(this.state.id)
+    fetch('http://api.sweet-travels.appspot.com/api/states/' + this.state.id)
       .then((response) => response.json())
       .then((responseJson) => {
         this.setState({
@@ -24,65 +37,31 @@ class StateDetail extends Component {
   render() {
     return (
       <div>
-        <CustomCard title={this.state.id}/>
+      <Container className="bg-faded p-4 my-4">
+        <hr className="divider"/>
+        <h2 className="text-center text-lg text-uppercase my-0">
+           <strong> {this.state.data.name}</strong>
+        </h2>
+        <hr className="divider"/>
+        <center><img width="50%" src={this.state.data.imageUrl} alt="parks image" /></center>
+        <p><b>Abbreviations:</b> {this.state.data.abbreviations}</p>
+        <p><b>Nicknames:</b> {this.state.data.nicknames}</p>
+        <p><b>Time Zone:</b> {this.state.data.timeZone}</p>
+        <p><b>Governor:</b> {this.state.data.governor}</p>
+        <p><b>Capital:</b> {this.state.data.capital}</p>
+        <p><b>Largest City:</b> {this.state.data.largestCity}</p>
+        <p><b>Total Population:</b> {this.state.data.totalPopulation}</p>
+        <p><b>Total Area:</b> {this.state.data.totalArea}</p>
+        <p><b>Median Income:</b> {this.state.data.medianIncome}</p>
+        <p><b>National Park:</b> {this.state.data.nationalParks}</p>
+        <p><b>Campground:</b> {this.state.data.campgrounds}</p>
+        <p><b>Website:</b> <a href={this.state.data.url}>{this.state.data.url}</a></p>
+        <p><b>Weather Info:</b> {this.state.data.weatherInfo}</p>
+        <p><b>Website:</b> <a href={this.state.data.url}>{this.state.data.url}</a></p>
+      </Container>
       </div>
     );
   }
 }
 
 export default StateDetail
-
-// import React, { Component } from 'react';
-// import {
-//   Container,
-//   Card,
-//   CardImg,
-//   CardText,
-//   CardBody,
-//   CardTitle
-// } from 'reactstrap';
-
-// const ca = require('./img/states/california.jpg');
-
-// function StateDetail(props) {
-//   return (
-//     <Card>
-//       <CardImg top width="100%" src={props.img} alt={props.alt} />
-//       <CardBody>
-//         <CardText>
-//           <b>Abbreviations: </b>{props.abbreviation}<br />
-//           <b>Nickname(s): </b>{props.nickname}<br />
-//           <b>Timezone: </b>{props.timezone}<br />
-//           <b>Governor: </b>{props.govenor}<br />
-//           <b>Capital: </b>{props.capital}<br />
-//           <b>Largest City: </b>{props.city}<br />
-//           <b>Total Population: </b>{props.population}<br />
-//           <b>Total Area: </b>{props.area}<br />
-//           <b>Median Household Income: </b>{props.income}<br />
-//           <b>National Park(s):</b> <a href={props.parkUrl}>{props.parkName}</a><br />
-//           <b>Campground(s): </b><a href={props.campgroundUrl}>{props.campgroundName}</a><br />
-//           <b>State website: </b><a href={props.stateUrl}>{props.stateUrl}</a>
-//         </CardText>
-//       </CardBody>
-//     </Card>
-//   );
-// }
-
-// export default function State(props) {
-//   return (
-//     <div>
-//       <Container className="bg-faded p-4 my-4">
-//         <hr className="divider"/>
-//         <h2 className="text-center text-lg text-uppercase my-0">California</h2>
-//         <hr className="divider"/>
-//           <StateDetail img={ca} alt="California Flag" abbreviation="CA, Calif., Cal." 
-//             nickname="The Golden State" timezone="Pacific (UTC −8/−7)" govenor="Jerry Brown (D)"
-//             capital="Sacramento" city="Los Angeles" population="39,250,017 (2016 est.)" 
-//             area="163,696 sq mi (423,970 km2)" income="$63,636"
-//             parkUrl="http://www.swetravels.me/parks/yosemite.html" parkName="Yosemite"
-//             campgroundUrl="http://www.swetravels.me/campgrounds/4508" campgroundName="Upper Pines"
-//             stateUrl="http://www.ca.gov"/>
-//       </Container>
-//     </div>
-//   );
-// }

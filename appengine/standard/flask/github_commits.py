0793044927b60ba4d6ab7e4b58ee89e38ad1412f
Katherine Bruton <kbruton@utexas.edu>
2017-10-26 21:08:24 -0500

    try again

--- appengine/standard/flask/github_commits.py
+++ appengine/standard/flask/github_commits.py
@@ -1,15 +1,15 @@
 import urllib2, json
 import requests
 
-endpoint = "https://api.github.com/repos/kbruton18/idb/stats/contributors"
-req = urllib2.Request(endpoint,headers={})
-
-response = urllib2.urlopen(req)
-the_page = response.read()
-the_page = the_page.decode("utf-8") 
-data = json.loads(the_page)
-
 def returnCommits(): 
+	endpoint = "https://api.github.com/repos/kbruton18/idb/stats/contributors"
+	req = urllib2.Request(endpoint,headers={})
+
+	response = urllib2.urlopen(req)
+	the_page = response.read()
+	the_page = the_page.decode("utf-8") 
+	data = json.loads(the_page)
+	
 	commit_list = {}
 	for x in data:
 		commit_dict = {}

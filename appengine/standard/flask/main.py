9fc8afcb3dca6bab674460835926e106da1215f3
Katherine Bruton <kbruton@utexas.edu>
2017-11-07 21:35:17 -0600

    Remove filter parameter to see if endpoint works again

--- appengine/standard/flask/main.py
+++ appengine/standard/flask/main.py
@@ -18,8 +18,9 @@ CORS(app)
 
 @app.route('/api/parks', methods=['GET'])
 def get_parks():
-  filter_values = request.headers['filter']
-  return jsonify(get_parks_list(filter_values))
+  # print(request.headers['filter'])
+  # filter_values = request.headers['filter']
+  return jsonify(get_parks_list())
 
 @app.route('/api/parks/<string:park_code>', methods=['GET'])
 def get_park(park_code):

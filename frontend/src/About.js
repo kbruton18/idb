8de1f4e74af1ea96dec6c185501cf091533a6043
Rachel Oei <racheloei@users.noreply.github.com>
2017-11-09 17:35:15 -0600

    Add technical report 3 to About page

--- frontend/src/About.js
+++ frontend/src/About.js
@@ -42,6 +42,7 @@ function AboutProcess (props) {
         The API for this site: <a href='http://docs.kbruton18.apiary.io'>Apiary</a><br />
         The technical report for phase 1 of the project: <a href='https://utexas.box.com/s/fwed2ij9san9fpwtrjrubv31k47jxznrt5ml5n8n6geae'>Report</a><br />
         The technical report for phase 2 of the project: <a href='https://utexas.box.com/s/fwed2ij9san9fpwtrjrubv31k47jxznr'>Report</a><br />
+        The technical report for phase 3 of the project: <a href='https://utexas.box.com/s/cc0tvmqt9b402ef38j45vj67glnuobv6'>Report</a><br />
         UML: <a href='https://utexas.box.com/s/m0ansejc67roe1x1hhngse049x2rddei'>UML</a>
       </p>
     </Container>

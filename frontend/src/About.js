941a7d02758bfe0524e9ab88108ed08103006935
Linh Nguyen <linh-nguyen@utexas.edu>
2017-11-28 21:11:48 -0600

    Updated about page to include links to new technical report and visulization

--- frontend/src/About.js
+++ frontend/src/About.js
@@ -43,8 +43,10 @@ function AboutProcess (props) {
         The technical report for phase 1 of the project: <a href='https://utexas.box.com/s/fwed2ij9san9fpwtrjrubv31k47jxznrt5ml5n8n6geae'>Report</a><br />
         The technical report for phase 2 of the project: <a href='https://utexas.box.com/s/fwed2ij9san9fpwtrjrubv31k47jxznr'>Report</a><br />
         The technical report for phase 3 of the project: <a href='https://utexas.box.com/s/cc0tvmqt9b402ef38j45vj67glnuobv6'>Report</a><br />
-        UML for phase 3: <a href='https://utexas.box.com/s/bihmk95dnoe15050dnifg3ae5jj94skn'>UML</a>
+        The technical report for phase 4 of the project: <a href='https://racheloei.gitbooks.io/sweet-travels'>Report</a><br />
+        UML for phase 4: <a href='https://utexas.box.com/s/bihmk95dnoe15050dnifg3ae5jj94skn'>UML</a><br />
       </p>
+      <p>Visualization for Group 19: <a href='http://swetravels.me/vis.html'>Link</a></p>
     </Container>
   );
   return (
